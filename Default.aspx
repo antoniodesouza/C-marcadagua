17c07ba39b25020a0796ab6778f954fbb9b9d3b9
Antonio de Souza <contato@antoniodesouza.com.br>
2014-10-31 02:08:15 -0200

    Upd.

---
+++ Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <label>
+            Imagem<br />
+            <asp:FileUpload runat="server" ID="ImagemFileUpload" /><br />
+
+            Logo<br />
+            <asp:FileUpload runat="server" ID="LogoFileUpload" /><br />
+
+            <asp:Button runat="server" ID="SalvarButton" OnClick="SalvarButton_Click" Text="Salvar" />
+
+        </label>
+
+
+        <hr />
+
+        <label>
+            Imagem<br />
+            <asp:TextBox runat="server" ID="ImagemTextBox" />
+
+            Logo<br />
+            <asp:TextBox runat="server" ID="LogoTextBox" />
+
+            <asp:Button runat="server" ID="SalvarComURLButton" OnClick="SalvarComURLButton_Click" Text="Salvar" />
+
+        </label>
+
+
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void SalvarButton_Click(object sender, EventArgs e)
+    {
+        String Pasta = Server.MapPath("./") + "\\Geracao";
+        String URLsaida = String.Format("{0}\\{1}.png", Pasta, Guid.NewGuid().ToString());
+        if (!Directory.Exists(Pasta)) Directory.CreateDirectory(Pasta);
+        Uteis.GeraMarcaDagua( Uteis.byteArrayToImage(ImagemFileUpload.FileBytes), Uteis.byteArrayToImage(LogoFileUpload.FileBytes), URLsaida, 900, 600);
+    }
+   
+    protected void SalvarComURLButton_Click(object sender, EventArgs e)
+    {
+        String Pasta = Server.MapPath("./") + "\\Geracao";
+        String URLsaida = String.Format("{0}\\{1}.png", Pasta, Guid.NewGuid().ToString());
+        if (!Directory.Exists(Pasta)) Directory.CreateDirectory(Pasta);
+
+
+        Uteis.GeraMarcaDagua(ImagemTextBox.Text, LogoTextBox.Text , URLsaida, 900, 600);
+
+    }
+}
